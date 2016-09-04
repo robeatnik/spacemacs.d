d460599e5325c01083ea79c1115071e4ed1e4396
robeatnik <beatnikrobinson@outlook.com>
2016-09-04 21:33:55 +0800

    enable fcitx in EN linux system

--- init.el
+++ init.el
@@ -272,71 +272,71 @@ you should place your code here."
 
   ;; set English and Chinese fonts and their sizes. from the readme of the layer
   ;; of chinese.
-  (spacemacs//set-monospaced-font "Monaco" "WenQuanYi Micro Hei Mono" 15 16)
+  (spacemacs//set-monospaced-font "Monospace" "WenQuanYi Micro Hei Mono" 15 16)
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; added by me
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  
- (require 'mu4e)
- 
- ;; default
- ;; (setq mu4e-maildir "~/Maildir")
- 
- (setq mu4e-drafts-folder "/[Gmail].Drafts")
- (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
- (setq mu4e-trash-folder  "/[Gmail].Trash")
- 
- ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
- (setq mu4e-sent-messages-behavior 'delete)
- 
- ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
- ;; additional non-Gmail addresses and want assign them different
- ;; behavior.)
- 
- ;; setup some handy shortcuts
- ;; you can quickly switch to your Inbox -- press ``ji''
- ;; then, when you want archive some messages, move them to
- ;; the 'All Mail' folder by pressing ``ma''.
- 
- (setq mu4e-maildir-shortcuts
-     '( ("/INBOX"               . ?i)
-        ("/[Gmail].Sent Mail"   . ?s)
-        ("/[Gmail].Trash"       . ?t)
-        ("/[Gmail].All Mail"    . ?a)))
- 
- ;; allow for updating mail using 'U' in the main view:
- (setq mu4e-get-mail-command "offlineimap")
- 
- ;; something about ourselves
- (setq
-    user-mail-address "beatnirobinson@gmail.com"
-    user-full-name  "sike"
-    mu4e-compose-signature
-     (concat
-       ;"Foo X. Bar\n"
-       ;"http://www.example.com\n"
-      ))
- 
- ;; sending mail -- replace USERNAME with your gmail username
- ;; also, make sure the gnutls command line utils are installed
- ;; package 'gnutls-bin' in Debian/Ubuntu
- 
- (require 'smtpmail)
- (setq message-send-mail-function 'smtpmail-send-it
-    starttls-use-gnutls t
-    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-    smtpmail-auth-credentials
-      '(("smtp.gmail.com" 587 "beatnirobinson@gmail.com" nil))
-    smtpmail-default-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-service 587)
- 
- ;; alternatively, for emacs-24 you can use:
- ;;(setq message-send-mail-function 'smtpmail-send-it
- ;;     smtpmail-stream-type 'starttls
- ;;     smtpmail-default-smtp-server "smtp.gmail.com"
- ;;     smtpmail-smtp-server "smtp.gmail.com"
- ;;     smtpmail-smtp-service 587)
+;; (require 'mu4e)
+;; 
+;; ;; default
+;; ;; (setq mu4e-maildir "~/Maildir")
+;; 
+;; (setq mu4e-drafts-folder "/[Gmail].Drafts")
+;; (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+;; (setq mu4e-trash-folder  "/[Gmail].Trash")
+;; 
+;; ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+;; (setq mu4e-sent-messages-behavior 'delete)
+;; 
+;; ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+;; ;; additional non-Gmail addresses and want assign them different
+;; ;; behavior.)
+;; 
+;; ;; setup some handy shortcuts
+;; ;; you can quickly switch to your Inbox -- press ``ji''
+;; ;; then, when you want archive some messages, move them to
+;; ;; the 'All Mail' folder by pressing ``ma''.
+;; 
+;; (setq mu4e-maildir-shortcuts
+;;     '( ("/INBOX"               . ?i)
+;;        ("/[Gmail].Sent Mail"   . ?s)
+;;        ("/[Gmail].Trash"       . ?t)
+;;        ("/[Gmail].All Mail"    . ?a)))
+;; 
+;; ;; allow for updating mail using 'U' in the main view:
+;; (setq mu4e-get-mail-command "offlineimap")
+;; 
+;; ;; something about ourselves
+;; (setq
+;;    user-mail-address "beatnirobinson@gmail.com"
+;;    user-full-name  "sike"
+;;    mu4e-compose-signature
+;;     (concat
+;;       ;"Foo X. Bar\n"
+;;       ;"http://www.example.com\n"
+;;      ))
+;; 
+;; ;; sending mail -- replace USERNAME with your gmail username
+;; ;; also, make sure the gnutls command line utils are installed
+;; ;; package 'gnutls-bin' in Debian/Ubuntu
+;; 
+;; (require 'smtpmail)
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;    starttls-use-gnutls t
+;;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;    smtpmail-auth-credentials
+;;      '(("smtp.gmail.com" 587 "beatnirobinson@gmail.com" nil))
+;;    smtpmail-default-smtp-server "smtp.gmail.com"
+;;    smtpmail-smtp-server "smtp.gmail.com"
+;;    smtpmail-smtp-service 587)
+;; 
+;; ;; alternatively, for emacs-24 you can use:
+;; ;;(setq message-send-mail-function 'smtpmail-send-it
+;; ;;     smtpmail-stream-type 'starttls
+;; ;;     smtpmail-default-smtp-server "smtp.gmail.com"
+;; ;;     smtpmail-smtp-server "smtp.gmail.com"
+;; ;;     smtpmail-smtp-service 587)
  
  ;; don't keep message buffers around
  (setq message-kill-buffer-on-exit t)
@@ -355,7 +355,8 @@ you should place your code here."
  (add-hook 'org-mode-hook
            (lambda ()
              (setq truncate-lines nil)))
- 
+ (global-set-key (kbd "C-SPC") 'nil)
+;;solve Fontset `tty' does not exist while turn on emacs in terminal via `emacs -nw'
  )
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
